e6c1b646e49a342a9eb0b0b5b4a41f108659bf03
Aky Natam <akynatan@outlook.com>
2023-05-06 21:56:54 -0300

    fixing https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: '/api',
   validateStatus: (status: number) => {
     if (status >= 200 && status < 300) {
       return true;
